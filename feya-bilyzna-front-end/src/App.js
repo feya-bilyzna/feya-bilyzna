2c626083548326edd9a62377c5902db6739e160a
AlecCross <cross2472@gmail.com>
2021-06-18 23:36:11 +0300

    init

---
+++ feya-bilyzna-front-end/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+        <>
+          <h1>feya-bilyzna.github.io</h1>
+        </>
+  );
+}
+
+export default App;
