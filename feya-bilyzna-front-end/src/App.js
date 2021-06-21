8153f81fe287b0de9ab99f8dc9b1b152d13506e6
AlecCross <cross2472@gmail.com>
2021-06-21 11:16:12 +0300

    Routes + Navbar
    ...Добавил стили materializecss; Создал набросок навбара с главной страницей и контактами, при переходе на странцы отображаются заголовки страниц

--- feya-bilyzna-front-end/src/App.js
+++ feya-bilyzna-front-end/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
+import {useRoutes} from "./routes";
+import 'materialize-css'
 
 function App() {
-  return (
-        <>
-          <h1>feya-bilyzna.github.io</h1>
-        </>
-  );
+    const routes = useRoutes()
+    return (
+        <BrowserRouter>
+            <div className="container">
+                {routes}
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
