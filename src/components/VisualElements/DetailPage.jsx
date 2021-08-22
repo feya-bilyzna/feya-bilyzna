5b4056aed4b7454ba2b5abaad423a16b6e635660
Andrew <life.around.freaks@gmail.com>
2021-08-22 05:24:18 +0300

    Страница товара. Добавил селекторы вариантов и сделал небольшие правки.

--- src/components/VisualElements/DetailPage.jsx
+++ src/components/VisualElements/DetailPage.jsx
@@ -4,31 +4,158 @@ import {useParams} from 'react-router';
 import {gql, useQuery} from "@apollo/client";
 import { LoadingAnimation } from '..';
 import { alertsData } from "../../data"
+import cx from 'classnames'
+
+
+const VariantSelectors = ({ selectorsData, updateSelector }) => {
+    return (
+        Object.entries(selectorsData).map(
+            ([selectorType, selectors]) =>
+                <Fragment key={selectorType}>
+                    <Row
+                        style={{ marginBottom: 10, marginTop: 10 }}
+                    >
+                        {selectors.map(selector =>
+                            <Button key={selector.value}
+                                floating
+                                style={{ margin: 3 }}
+                                disabled={selector.disabled}
+                                className={cx(
+                                    selectorType === "color" ? selector.value : "red lighten-2",
+                                    { pulse: selector.selected })
+                                }
+                                onClick={() => updateSelector(selectorType, selector.value)}
+                            >{selectorType === "color" ? "" : selector.value}</Button>
+                        )}
+                    </Row>
+                    <Divider />
+                </Fragment>
+        )
+    )
+}
 
 
 const DetailPage = () => {
 
-    const {productId} = useParams()
     const ProductQuery = gql`
-        query ProductQuery($id: Int!) {
-          productById(id: $id) {
+    query ProductQuery($id: Int!) {
+        productById(id: $id) {
+            categories
             description
-            name
-            images {
-              url
-              id
-            }
             id
+            images {
+              id
+              url
+            }
+            name
+            remains {
+              price
+              variantId
+              variantName
+              variantStyle
+              remains
+            }
+            brandName
           }
         }
     `
 
-    const {loading, error, data} = useQuery(ProductQuery, {
-        variables: {id: productId},
+    const [selectedOptions, setselectedOptions] = useState({})
+    const { productId } = useParams()
+    const { loading, error, data } = useQuery(ProductQuery, {
+        variables: { id: productId },
     })
 
-    if (loading) return <LoadingAnimation style={{height: "50vh"}} />
+    if (loading) return <LoadingAnimation style={{ height: "50vh" }} />
     if (error) return <h5 style={{ textAlign: "center" }}>{alertsData.serverRequestFailed}</h5>
+
+    const allowedSelectors = new Set(['color', 'size', 'bandSize', 'cupSize'])
+    const updateSelector = (selector, value) => {
+        if (allowedSelectors.has(selector)) {
+            if (selectedOptions[selector] === value) {
+                setselectedOptions({ ...selectedOptions, [selector]: undefined })
+            } else setselectedOptions({ ...selectedOptions, [selector]: value })
+        }
+    }
+
+    const variantIsValid = variant => variant && Object.keys(variant).map(
+        value => allowedSelectors.has(value)
+    ).every(value => value)
+
+    const intersection = sets => sets.reduce(
+        (intersected, set) => new Set([...set].filter(x => intersected.has(x)))
+    )
+
+    const variantsData = {}
+    const combinedVariants = {}
+    const addOrCreate = (selector, value, relatedSelector, relatedValue) => {
+        if (variantsData[selector] === undefined)
+            variantsData[selector] = {}
+        if (variantsData[selector][value] === undefined)
+            variantsData[selector][value] = {}
+        if (variantsData[selector][value][relatedSelector] === undefined)
+            variantsData[selector][value][relatedSelector] = new Set()
+        variantsData[selector][value][relatedSelector].add(relatedValue)
+
+        if (combinedVariants[selector] === undefined)
+            combinedVariants[selector] = new Set()
+        combinedVariants[selector].add(value)
+    }
+
+    const variants = data.productById.remains.filter(
+        remains => remains.remains
+    ).map(remains => remains.variantStyle)
+    for (let variant of variants) {
+        if (variantIsValid(variant)) {
+            for (let [name, value] of Object.entries(variant)) {
+                let { [name]: current, ...otherSelectors } = variant
+                for (let [relatedName, relatedValue] of Object.entries(otherSelectors)) {
+                    addOrCreate(name, value, relatedName, relatedValue)
+                }
+            }
+        }
+    }
+
+    const optionIsDisabled = (selector, option) => {
+        if (selectedOptions[selector] && selectedOptions[selector] !== option) return true
+        let { [selector]: current, ...related } = variantsData
+
+        return !intersection(Object.entries(related).map(
+            ([relatedSelector, relatedData]) => {
+                if (selectedOptions[relatedSelector] === undefined) return combinedVariants[selector]
+                return relatedData[selectedOptions[relatedSelector]][selector]
+            }
+        )).has(option)
+    }
+
+    const unfilteredSelectorsData = {}
+    for (let selector of Object.keys(variantsData)) {
+        unfilteredSelectorsData[selector] = [...combinedVariants[selector]].map(
+            option => {
+                return {
+                    value: option,
+                    selected: selectedOptions[selector] === option,
+                    disabled: optionIsDisabled(selector, option),
+                }
+            }
+        )
+    }
+    const selectorsData = Object.fromEntries(
+        Object.entries(unfilteredSelectorsData).filter(
+            ([selector, options]) => options.length > 1
+        )
+    )
+
+    const appropriateRemains = data.productById.remains.filter(
+        remains => remains.remains && Object.entries(remains.variantStyle).reduce(
+            (allAppropriate, [styleName, styleValue]) =>
+                allAppropriate && (
+                    selectedOptions[styleName] === undefined ||
+                    selectedOptions[styleName] === styleValue
+                ),
+            true
+        )
+    )
 
     return (
         <Row>
@@ -39,24 +166,26 @@ const DetailPage = () => {
                 s={12}
             >
                 {Object.values(data?.productById.images).map(image =>
-                    <MediaBox
+                    <div
                         key={image.id}
-                        options={{
-                            inDuration: 250,
-                            outDuration: 200
-
-                        }}
-                    >
-                        <img style={{
+                        style={{
                             marginTop: 15,
                             boxShadow: "0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%)",
                             borderRadius: "2px"
-                        }}
-                             alt="Изображение товара"
-                             src={image.url}
-                             width="100%"
-                        />
-                    </MediaBox>
+                        }}>
+                        <MediaBox
+                            options={{
+                                inDuration: 250,
+                                outDuration: 200
+                            }}
+                        >
+                            <img
+                                alt="Изображение товара"
+                                src={image.url}
+                                width="100%"
+                            />
+                        </MediaBox>
+                    </div>
                 )}
             </Col>
             <Col
