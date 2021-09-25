4c3313c90ca3f03a08c80367885d727133cf2ac8
AlecCross <cross2472@gmail.com>
2021-09-25 16:58:34 +0300

    Более декларативный способ обращения к contactsData
    Заполнил пустые altы
    
    вынес ссылки для изображений слайдов на главной в дату

--- src/data/index.js
+++ src/data/index.js
@@ -1,2 +1,3 @@
 export {default as categoriesData} from './categoriesData'
 export {default as alertsData} from './alertsData'
+export {default as sliderData} from './sliderData'
---
+++ src/data/sliderData.js
@@ -0,0 +1,24 @@
+const sliderData = {
+    links: [
+        {
+            link: "https://bn1301files.storage.live.com/y4mIyFICCQ_K6jj7K0BHd_C4_TZ3IXM7gh3nctLnvFRjvovSWun-WKv4tDzy7SD0awK7z7ajgpyFssdRMFJx6GK3QcWDjAc3siuENmK1UTs8xwjbkxWZdAI-qo8a3vYx1xAXFivZIoUuJFYryhY4IjZQsbp6zLZLnuOKXfTUCgLacNqJZ5dXVaAgB10HUdblF-Y?width=1400&height=800&cropmode=none"
+        },
+        {
+            link: "https://bn1301files.storage.live.com/y4mGjyhtjZGgG-YcHhqawiwhZfn0bpegVKN5IosGWsp09RRvcUj7VKVIBXPDV1B0NLIUMhQq4Qkm3sFbC5qmYo4tpDrHIT7HrmR36Lvocb8wkDjY0BhBvpbNLX6k4-JxMgod2eKil7TrsxJe9D_Iqp-nNXxQN0p61iKs-vnTJqChE14Cz6XuphJpsUrdLyBMN8x?width=660&height=440&cropmode=none"
+        },
+        {
+            link: "https://bn1301files.storage.live.com/y4m8abZ9U_g7OiiVFOABVk8G7b5yfgdTF80HUDPZJn5xlGS-YNqmToHQMyhEL4W2z6fiqNUPP7aOXeMTB_b0KPd2_pa0bmUbmP1aXvao161wz44Fm2xH5CG6hrSvxa8CjGKRINP2NTFc1wH01mWGOaNyddJfveWQAariYEZfk8-9lSbvlKJhQOTsTpXtwqIKdYL?width=519&height=320&cropmode=none"
+        },
+        {
+            link: "https://bn1301files.storage.live.com/y4mwjz62nP_ULmRkyTOQN95B-b4W0UrVofrH1dyUvWK7rlaff3fcKt1Chu55KctkldrVjL3Q5fFLJmw0inEXZfuzWPnGVdaIX6Zpqr-0_M2g1ieYt7k2nuq4lXNhQpQCA_kUn9o-61Si37vsCMzVaZKTaFYDNrgXNK8BZbyTnDKcJQvXjpDynbvDsoLMWCMNkiP?width=660&height=346&cropmode=none"
+        },
+        {
+            link: "https://bn1301files.storage.live.com/y4mR_b-XmnB1K0HDq1dZC4-aTtLPiC7omlOGSOAEGcOxWnjNLF2kPmKVe3Wa-yUNBbaG3km4ce5CogaF8gOd5PmPzmG0IPFwpG8oiviYPVWUvlSjqu_FI3V-p8ydW9OMA4Hgx_90Q4NzAEcoQ5YwnOLu3YilFMhLxKuVU9yApqTbxSpuguQSDXmae4NxF0lsQg1?width=600&height=295&cropmode=none"
+        },
+        {
+            link: "https://bn1301files.storage.live.com/y4mkzYESjT2dZEaAr3HcXVuYf21_DCeMiCgdBKc5YQ9KAN7P3GKKW4RWR6g5fe7rm8bL5y1gjXoQLaJxGzlVa_XCT15zhpuhEAEqKaMnhmaF0qT75aP4-oNOUuDmcXc29zPOsPOCxIaihZVQIT4EzRXGAB7CVpY2Sm2LPCkIvksqI800d05lyTOtzLltT4yDpb_?width=256&height=256&cropmode=none"
+        },
+    ]
+}
+
+export default sliderData
