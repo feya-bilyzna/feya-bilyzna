75b577a285e334f84b62fea7273411e956d36f5b
AlecCross <cross2472@gmail.com>
2021-10-06 20:52:12 +0300

    StickyNavbar, Отображение кол-ва товаров на иконке корзины навбара

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -3,6 +3,7 @@ import {NavLink} from "react-router-dom"
 import {Col, Dropdown, Icon, Navbar, Row} from "react-materialize"
 import PropTypes from "prop-types"
 import {categoriesData} from "../data"
+import {useCookies} from "react-cookie";
 
 Navbar.propTypes = {
     ...Navbar.propTypes,
@@ -10,9 +11,14 @@ Navbar.propTypes = {
 }
 
 const NavbarMenu = () => {
-
+    const [cookies] = useCookies(['cartProducts'])
+    const cartSize = Object.keys(cookies.cartProducts || {}).length
     return <Navbar
-        className={"pink accent-4"}
+        className={"pink accent-4 z-depth-2"}
+        style={{
+            position: "fixed",
+            top: 0,
+            zIndex: 3}}
         brand={
             <><NavLink className="brand-logo" to="/"
             >
@@ -28,20 +34,25 @@ const NavbarMenu = () => {
                     </Col>
                     <div style={{overflow: "hidden"}}>
                         <Col style={{paddingRight: 25}}
-                            s={12}
-                            m={6}
-                            l={6}
-                            xl={6}
+                             s={12}
+                             m={6}
+                             l={6}
+                             xl={6}
                         >
                             Feya
                         </Col>
                     </div>
-
                 </Row>
             </NavLink>
-            <NavLink to="/cart" style={{ position: "absolute", right: 20 }}>
-                <Icon>shopping_cart</Icon>
-            </NavLink></>}
+                <NavLink to="/cart"
+                         style={{position: "absolute", right: 0, padding: "0 13px 0 13px"}}>
+                    <Icon>shopping_cart</Icon>
+                    {cartSize > 0 ? <span
+                        style={{color: "white", position: "absolute", top: -10, left: 34,}}
+                    >
+                        {cartSize}
+                </span> : <></>}
+                </NavLink></>}
         centerChildren
         id="mobile-nav"
         menuIcon={<Icon>menu</Icon>}
@@ -50,7 +61,8 @@ const NavbarMenu = () => {
             <>
                 <NavLink to="/">
                     <div className="background">
-                        <img style={{width: "100%"}} alt="Логотип" src="https://bn1301files.storage.live.com/y4moe3hVn4paJgAFL1jDAEl3VyJrCawq4hIPSPdmDwHMpMKqa85lvHgqVnezDlNAICbnSeMxOK1eMR2As3uVkDEi8IfzhphnGhcdOqSE_ecGzisHPFLus1GgFdzzwej_e-yTbWHfAucXe7Qfo1mj9SPjYALn8vYrSWUWBbAeHIQ8UugG-6QoXAuWPrB6y9NT4aa?width=256&height=256&cropmode=none"/>
+                        <img style={{width: "100%"}} alt="Логотип"
+                             src="https://bn1301files.storage.live.com/y4moe3hVn4paJgAFL1jDAEl3VyJrCawq4hIPSPdmDwHMpMKqa85lvHgqVnezDlNAICbnSeMxOK1eMR2As3uVkDEi8IfzhphnGhcdOqSE_ecGzisHPFLus1GgFdzzwej_e-yTbWHfAucXe7Qfo1mj9SPjYALn8vYrSWUWBbAeHIQ8UugG-6QoXAuWPrB6y9NT4aa?width=256&height=256&cropmode=none"/>
                     </div>
                 </NavLink>
                 {Object.values(categoriesData.categories).map(categoryData =>
@@ -109,7 +121,7 @@ const NavbarMenu = () => {
             )}
         </Dropdown>
         <NavLink to="/contacts">Контакты</NavLink>
-        <div style={{ marginRight: 50 }} />
+        <div style={{marginRight: 50}}/>
     </Navbar>
 }
 
--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const PageContainer = ({children}) => {
     return <div
-        className="container" style={{
+        className="container z-depth-1" style={{
         flex: 1,
         backgroundColor: "white",
-        marginTop: "1.2rem",
-        boxShadow: "rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 3px 1px -2px, rgba(0, 0, 0, 0.2) 0px 1px 5px 0px"
+        marginTop: "5.5rem",
     }}
     >
         {children}
