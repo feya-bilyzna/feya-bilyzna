666b565706e99f3187d049d304a2cd269c20efed
AlecCross <cross2472@gmail.com>
2021-10-10 06:17:59 +0300

    Добален модульнный css файл для PageContainer с медиа запросами
    Если не получается обойтись инлайн стилями, все равно не стоит использовать статику, вместо этого лучше использовать модульные стили.

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import s from '../css.module/PageContainer.module.css'
+import cx from "classnames";
 
 const PageContainer = ({children}) => {
     return <div
-        className="container z-depth-1" style={{
+        className={cx(s.container, "z-depth-1")}
+        style={{
         flex: 1,
         backgroundColor: "white",
         zIndex: 1
