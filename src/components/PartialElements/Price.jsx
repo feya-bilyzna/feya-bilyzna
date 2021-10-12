2e3f892d3d148da7431276fb5b5a4a310423469c
AlecCross <cross2472@gmail.com>
2021-10-12 05:43:20 +0300

    Проверка на наличие цены у товара

--- src/components/PartialElements/Price.jsx
+++ src/components/PartialElements/Price.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Price = ({remains}) => {
+    const noPrice = "Нет цены"
     return <>
-        {remains ? <div
+        {remains ? remains[0] ? <div
             className="pink-text accent-4"
-        >{remains[0].price ? `${remains[0].price} грн` : "Нет цены"}</div> : <></>}
+        >{remains[0].price ? `${remains[0].price} грн` : noPrice}</div> : <></>:noPrice}
     </>
 }
 
