27dfcf62bf9ab13d91cbadde6f6646c59624fb2e
AlecCross <cross2472@gmail.com>
2021-11-16 15:06:29 +0200

    Добавлены иконки для Контактов и Все товары на навбаре

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {NavLink} from "react-router-dom"
-import {Col, Dropdown, Navbar, Row} from "react-materialize"
+import { NavLink } from "react-router-dom"
+import { Col, Dropdown, Navbar, Row } from "react-materialize"
 import PropTypes from "prop-types"
-import {categoriesData} from "../data"
-import {CustomIcon, LoginButton} from "./index";
-import {useCookies} from "react-cookie";
+import { categoriesData } from "../data"
+import { CustomIcon, LoginButton } from "./index";
+import { useCookies } from "react-cookie";
 import styles from "../css/navbar.module.css"
 import cx from "classnames";
 
@@ -17,28 +17,28 @@ const NavbarMenu = () => {
     const [cookies] = useCookies(['cartProducts'])
     const cartSize = Object.keys(cookies.cartProducts || {}).length
 
-    return <Navbar style={{zIndex: 3}}
+    return <Navbar style={{ zIndex: 3 }}
         className={"pink accent-4"}
         brand={
             <>
                 <NavLink className={cx("brand-logo", styles.navElement)} to="/"
                 >
-                    <Row style={{marginBottom: 0, marginRight: 20}}>
+                    <Row style={{ marginBottom: 0, marginRight: 20 }}>
                         <Col
                             s={12}
                             m={6}
                             l={6}
                             xl={6}
-                            style={{display: "flex", justifyContent: "center"}}
+                            style={{ display: "flex", justifyContent: "center" }}
                         >
-                            <CustomIcon large style={{margin: 0}}>home</CustomIcon>
+                            <CustomIcon large style={{ margin: 0 }}>home</CustomIcon>
                         </Col>
-                        <div style={{overflow: "hidden"}}>
-                            <Col style={{paddingRight: 25}}
-                                 s={12}
-                                 m={6}
-                                 l={6}
-                                 xl={6}
+                        <div style={{ overflow: "hidden" }}>
+                            <Col style={{ paddingRight: 25 }}
+                                s={12}
+                                m={6}
+                                l={6}
+                                xl={6}
                             >
                                 Feya
                             </Col>
@@ -52,10 +52,10 @@ const NavbarMenu = () => {
                 }}>
                     <CustomIcon>shopping_cart</CustomIcon>
                     {cartSize > 0 ? <span
-                        style={{color: "white", position: "absolute", top: -10, left: 34,}}
+                        style={{ color: "white", position: "absolute", top: -10, left: 34, }}
                     >
                         {cartSize}
-                </span> : <></>}
+                    </span> : <></>}
                 </NavLink>
             </>
         }
@@ -67,10 +67,10 @@ const NavbarMenu = () => {
             <>
                 <NavLink to="/">
                     <div className="background">
-                        <img style={{width: "100%"}} alt="Логотип" src="https://bn1301files.storage.live.com/y4moe3hVn4paJgAFL1jDAEl3VyJrCawq4hIPSPdmDwHMpMKqa85lvHgqVnezDlNAICbnSeMxOK1eMR2As3uVkDEi8IfzhphnGhcdOqSE_ecGzisHPFLus1GgFdzzwej_e-yTbWHfAucXe7Qfo1mj9SPjYALn8vYrSWUWBbAeHIQ8UugG-6QoXAuWPrB6y9NT4aa?width=256&height=256&cropmode=none"/>
+                        <img style={{ width: "100%" }} alt="Логотип" src="https://bn1301files.storage.live.com/y4moe3hVn4paJgAFL1jDAEl3VyJrCawq4hIPSPdmDwHMpMKqa85lvHgqVnezDlNAICbnSeMxOK1eMR2As3uVkDEi8IfzhphnGhcdOqSE_ecGzisHPFLus1GgFdzzwej_e-yTbWHfAucXe7Qfo1mj9SPjYALn8vYrSWUWBbAeHIQ8UugG-6QoXAuWPrB6y9NT4aa?width=256&height=256&cropmode=none" />
                     </div>
                 </NavLink>
-                <li><LoginButton sideNav={true}/></li>
+                <li><LoginButton sideNav={true} /></li>
                 {Object.values(categoriesData.categories).map(categoryData =>
                     <li key={categoryData.route}>
                         <NavLink to={categoryData.route}>{categoryData.name}</NavLink>
@@ -113,7 +113,10 @@ const NavbarMenu = () => {
                 outDuration: 250,
             }}
 
-            trigger={<a href="#!">Все товары{' '}</a>}
+            trigger={<a style={{ display: "flex" }} href="#!">
+                <CustomIcon>arrow_drop_down</CustomIcon>
+                Все товары
+            </a>}
         >
             {Object.values(categoriesData.categories).map(categoryData =>
                 <NavLink key={categoryData.route} to={categoryData.route}>
@@ -126,8 +129,11 @@ const NavbarMenu = () => {
                 </NavLink>
             )}
         </Dropdown>
-        <NavLink to="/contacts">Контакты</NavLink>
-        <LoginButton cardButton={false}/>
+        <NavLink style={{ display: "flex" }} to="/contacts">
+            <CustomIcon>phone_in_talk</CustomIcon>
+            Контакты
+        </NavLink>
+        <LoginButton cardButton={false} />
         <div style={{ marginRight: 50 }} />
     </Navbar>
 }
