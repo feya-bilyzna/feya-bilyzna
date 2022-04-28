f1f27d756cff996dcb4c20f42fe2d6c797b681a4
AlecCross <cross2472@gmail.com>
2022-04-28 10:40:17 +0300

    Правки по PR - Отметка на товаре ПРОДАНО

--- src/components/CustomMaterialize/CustomCard.js
+++ src/components/CustomMaterialize/CustomCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { NavLink } from "react-router-dom"
 import { ImageView, Price, Sale } from ".."
 import { useTranslation } from "react-i18next"
@@ -6,7 +6,7 @@ import { categoriesData } from '../../data';
 import WishlistButton from '../PartialElements/WishlistButton'
 import { useCookies } from "react-cookie"
 
-const CustomCard = ({ item, image, isSubcategory }) => {
+const CatdBody = ({ item, image, isSubcategory }) => {
     const customCardContentStyle = {
         whiteSpace: "nowrap",
         overflow: "hidden",
@@ -15,8 +15,28 @@ const CustomCard = ({ item, image, isSubcategory }) => {
         color: "black",
         padding: "2% 0px 3%",
     }
+    const { t } = useTranslation()
+    return <>
+        <div className="card-image">
+            <ImageView image={image} />
+        </div>
+        {isSubcategory && item.categories.includes(categoriesData.uncategorizedSubcategories.sale.name) ?
+            <Sale /> :
+            <></>}
+        <div className="card-content flow-text" style={customCardContentStyle}>
+            {(item.vendorCode || item.brandName) ?
+                `${item.vendorCode}•${item.brandName || t("Нет бренда")}` :
+                t(item.name)
+            }
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                {isSubcategory ? <Price item={item} /> : <></>}
+            </div>
+        </div>
+    </>
+}
 
-    const salesStyle = {
+const CustomCard = ({ item, image, isSubcategory }) => {
+    const soldTextStyle = {
         background: "linear-gradient(rgba(0,0,0,0.6) 50%, rgba(255,255,255,0) 100%)",
         color: "white",
         position: "absolute",
@@ -33,52 +53,33 @@ const CustomCard = ({ item, image, isSubcategory }) => {
 
     const [cookies] = useCookies(['wishlist'])
 
-    const { t } = useTranslation()
-
     const wishlist = new Set(Object.values(cookies.wishlist || []))
 
-    const areAvailable = (remains) => {
-        let remainLength = 0
-        remains.map(remain => {
-            if (remain.remains === 0) remainLength++
-            return null
-        })
-        if (remainLength === remains.length) return true
-        else return false
-    }
+    const productIsSold = isSubcategory && item.remains.reduce(
+        (prev, item) => prev + item.remains, 0
+    ) === 0
 
-    const handleClick = e => e.preventDefault()
+    const { t } = useTranslation()
 
-    return <div className={isSubcategory && areAvailable(item.remains) ? "card" : "card hoverable"}
+    return <div
+        className={productIsSold ? "card" : "card hoverable"}
         style={{ margin: ".5rem 0 .1rem 0" }}>
         {isSubcategory ? <WishlistButton
             wishlist={wishlist}
             id={item.id}
             isSubcategory={isSubcategory}
         /> : <></>}
-        <NavLink to={item.route ? item.route : `/${item.id}`}
-            onClick={isSubcategory && areAvailable(item.remains) ? handleClick : null}>
-            <div className="card-image">
-                <ImageView areAvailable={isSubcategory ? areAvailable(item.remains) : false} image={image} />
-            </div>
-            {isSubcategory && item.categories.includes(categoriesData.uncategorizedSubcategories.sale.name) ?
-                <Sale /> :
-                <></>}
-            <div className="card-content flow-text" style={customCardContentStyle}>
-                {(item.vendorCode || item.brandName) ?
-                    `${item.vendorCode}•${item.brandName || t("Нет бренда")}` :
-                    t(item.name)
-                }
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    {isSubcategory ? <Price item={item} /> : <></>}
-                </div>
-            </div>
-        </NavLink>
-        {
-            isSubcategory ? areAvailable(item.remains) ?
-                <div style={salesStyle}
+        {productIsSold ?
+            <>
+                <CatdBody item={item} image={image} isSubcategory={isSubcategory} />
+                <div style={soldTextStyle}
                     className="flow-text"
-                >ПРОДАНО</div> : <></> : <></>
+                >{t("ПРОДАНО")}</div>
+            </>
+            :
+            <NavLink to={item.route ? item.route : `/${item.id}`}>
+                <CatdBody item={item} image={image} isSubcategory={isSubcategory} />
+            </NavLink>
         }
     </div >
 }
