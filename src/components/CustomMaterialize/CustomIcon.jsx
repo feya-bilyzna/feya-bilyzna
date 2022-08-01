a65d849dd17f0b3fbad74ccc764af74940fca178
AlecCross <cross2472@gmail.com>
2022-08-01 17:40:30 +0300

    Исправления для pr111 замена div на span

--- src/components/CustomMaterialize/CustomIcon.jsx
+++ src/components/CustomMaterialize/CustomIcon.jsx
@@ -3,9 +3,9 @@ import { Icon } from "react-materialize";
 import cx from "classnames";
 
 const CustomIcon = ({ children, className, ...props }) => {
-   return <div data-nosnippet>
+   return <span data-nosnippet>
       <Icon className={cx("notranslate", className)} {...props}>{children}</Icon>
-   </div>
+   </span>
 };
 
 export default CustomIcon
